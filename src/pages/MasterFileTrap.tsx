a2569e7082578827acbe62ca38746282230c4769
hackthehackerss <HackTheHackres@gmail.com>
2025-02-17 22:55:59 +0200

    Update MasterFileTrap.tsx

--- src/pages/MasterFileTrap.tsx
+++ src/pages/MasterFileTrap.tsx
@@ -158,7 +158,8 @@ function MFTChallenge() {
           <div className="absolute inset-0 rounded-lg bg-primary-blue/40 blur-lg group-hover:opacity-100 opacity-0 transition-opacity duration-300"></div>
 
           <img
-            src="/Challenges/mft-banner.png"
+          
+            src="/src/assets/images/Challenges/mft-banner.png"
             alt="MFT Analysis Challenge Banner"
             className="w-auto max-h-80 object-cover rounded-lg shadow-lg group-hover:scale-105 group-hover:rotate-1 transition-transform duration-300 ease-in-out relative z-10"
           />
