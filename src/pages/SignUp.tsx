f3eecea001f00c28f75c879356edb72d7a2d3cf5
hackthehackerss <HackTheHackres@gmail.com>
2025-02-20 01:48:23 +0200

    Update SignUp.tsx

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, Loader2, Eye, EyeOff, AlertCircle } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
-import logoShieldImg from '../assets/images/logo-shield.png';
+import logoShieldImg from '/Main/logo-shield.png';
 
 interface FormErrors {
   email?: string;
