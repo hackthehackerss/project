c1df823f96e20395b1b913fe66cda05b0acbbe39
hackthehackerss <HackTheHackres@gmail.com>
2025-02-23 08:47:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,8 @@ import Search from './pages/Search';
 import NotFound from './pages/NotFound';
 import SubmitChallenge from './pages/SubmitChallenge';
 import Labs from './pages/Labs';
+import SqlLab from './pages/Labs/sql.tsx';
+import XssLab from './pages/Labs/xss.tsx';
 import EmailAnalysisChallenge from './pages/Challenges/EmailAnalysisChallenge'; 
 
 function App() {
@@ -64,6 +66,8 @@ function App() {
           <Route path="/search" element={<Search />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/labs" element={<Labs />} /> 
+          <Route path="/labs/sql.tsx" element={<SQLLabs />} /> 
+          <Route path="/labs/xss.tsx" element={<XSSLabs />} /> 
           <Route path="/challenges/email-analysis" element={<EmailAnalysisChallenge />} />
         </Routes>
       </Router>
